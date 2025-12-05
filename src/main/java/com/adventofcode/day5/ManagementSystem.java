21bf28a3b1f38b073adb3b63dd9102bfc2cfb311
michalantkowicz <michal@antkowicz.pl>
2025-12-05 23:10:45 +0100

    reformatting day5

--- src/main/java/com/adventofcode/day5/ManagementSystem.java
+++ src/main/java/com/adventofcode/day5/ManagementSystem.java
@@ -2,7 +2,6 @@ package com.adventofcode.day5;
 
 import com.adventofcode.common.Pair;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -14,22 +13,20 @@ class ManagementSystem {
     }
 
     long countAllFresh(List<Pair<Long>> ranges) {
+        long result = 0L;
         ranges = ranges.stream().sorted(Comparator.comparingLong(Pair::a)).toList();
-        List<Pair<Long>> effectiveRanges = new ArrayList<>();
         Pair<Long> current = ranges.getFirst();
-        for (int i = 1; i < ranges.size(); i++) {
-            Pair<Long> next = ranges.get(i);
+        for (Pair<Long> next : ranges.subList(1, ranges.size())) {
             if (next.a() <= current.b()) {
                 if (next.b() > current.b()) {
                     current = new Pair<>(current.a(), next.b());
                 }
             } else {
-                effectiveRanges.add(current);
+                result += length(current);
                 current = next;
             }
         }
-        effectiveRanges.add(current);
-        return effectiveRanges.stream().mapToLong(this::length).sum();
+        return result + length(current);
     }
 
     private long length(Pair<Long> range) {
